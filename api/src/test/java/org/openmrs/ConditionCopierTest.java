0878bd92c487812b2c4c2c538a40299793107e8e
txandmj <txandmj@outlook.com>
2025-03-03 21:58:17 -0800

    fix build

--- api/src/test/java/org/openmrs/ConditionCopierTest.java
+++ api/src/test/java/org/openmrs/ConditionCopierTest.java
@@ -1,11 +1,18 @@
-package org.openmrs;
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 
+package org.openmrs;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.Date;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
